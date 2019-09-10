54b1cf313ce7552e66d41719d29a276ea11fc3cf
Sander Shi <sander.hacker@hotmail.com>
2019-09-10 00:38:48 -0700

    Mobile Navigation Complete

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,15 +1,6 @@
 // Main Javascript
-console.log("Welcome to Nancy's Website!");
-
-
-// Typing animation on landing page
-var options = {
-    strings: [
-        "&nbsp;STUDENT",
-        "&nbsp;PRODUCT MANAGER",
-        "&nbsp;SOFTWARE ENGINEER",
-        "n ENVIRONMENTALIST"],
-    typeSpeed: 80,
-    backDelay: 1000,
-}
-var typed = new Typed(".element", options);
+$(document).ready(function () {
+    $("#nav-button").click(function () {
+        $("#mobile-nav").fadeToggle(200);
+    });
+});
---
+++ assets/js/typed_conf.js
@@ -0,0 +1,12 @@
+// Typing animation on landing page
+var options = {
+    strings: [
+        "&nbsp;STUDENT",
+        "&nbsp;PRODUCT MANAGER",
+        "&nbsp;SOFTWARE ENGINEER",
+        "n ENVIRONMENTALIST"],
+    typeSpeed: 80,
+    backDelay: 1000,
+    loop: true
+}
+var typed = new Typed(".element", options);

868d90b5182a3ddfe4495f267fecba5a776f528d
Sander Shi <sander.hacker@hotmail.com>
2019-09-10 20:55:17 -0700

    Sleeker and more convenient mobile navigation, raise social media buttons

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
     $("#nav-button").click(function () {
         $("#mobile-nav").fadeToggle(200);
     });
+    $(".link-wrapper").click(function () {
+        $("#mobile-nav").fadeToggle(200);
+    });
 });

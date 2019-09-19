c66bd350d1d138fdfe824802ea6797f0c95c2c62
Sander Shi <sander.shi@hulu.com>
2019-09-19 12:50:23 -0400

    Colour navbar on scroll, solves issue #1

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -6,4 +6,17 @@ $(document).ready(function () {
     $(".link-wrapper").click(function () {
         $("#mobile-nav").fadeToggle(200);
     });
+
+    // Navbar style
+    window.onscroll = () => {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            $("#navigation-bar").toggleClass("dark-background", true);
+        } else {
+            $("#navigation-bar").toggleClass("dark-background", false);
+        }
+    }
 });
